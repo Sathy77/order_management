7b460458786467fc0d5c5202790bc7501919635b
sathy.akter <sathy.akter@apisolutionsltd.com>
2025-01-27 11:15:31 +0600

    super-user

--- noauth/views.py
+++ noauth/views.py
@@ -8,14 +8,29 @@ from rest_framework import status
 @api_view(['GET'])
 def createUser(request, username=None, password=None):
     try:
+        # Create the user
         if username and password:
             user = MODELS_USER.User.objects.create_superuser(username=username, password=password)
-            user.user_type='Admin'
-            user.save()
-            return Response({'message': 'User created!', 'username': username, 'password': password}, status=status.HTTP_201_CREATED)
         else:
             user = MODELS_USER.User.objects.create_superuser(username='admin', password='admin')
-            user.user_type='Admin'
-            user.save()
-            return Response({'message': 'User created!', 'username': 'admin', 'password': 'admin'}, status=status.HTTP_201_CREATED)
-    except: return Response({'message': 'Couldn\'t create user!', 'username': '', 'password': ''}, status=status.HTTP_400_BAD_REQUEST)
+        
+        # Set user type
+        user.user_type = 'Admin'
+        user.save()
+        
+        # Assign all roles to the user
+        roles = MODELS_USER.Role.objects.all()
+        user.role.set(roles)
+        
+        return Response({
+            'message': 'User created!',
+            'username': username or 'admin',
+            'password': password or 'admin'
+        }, status=status.HTTP_201_CREATED)
+    except Exception as e:
+        # Return error response with the exception message
+        return Response({
+            'message': f"Couldn't create user! {str(e)}",
+            'username': '',
+            'password': ''
+        }, status=status.HTTP_400_BAD_REQUEST)
